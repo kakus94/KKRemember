af20ed8df831232056a1807f43ace69b492627f5
Kamil <kkarpiak94@icloud.com>
2023-03-09 22:45:21 +0100

    Initial Commit

---
+++ KKRemember/KKRememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  KKRememberApp.swift
+//  KKRemember
+//
+//  Created by Kamil Karpiak on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct KKRememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
